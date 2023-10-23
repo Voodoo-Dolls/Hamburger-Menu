8354feed85ab0a0dc6ca20f762783b84d331c9b4
Michael <92550503+Voodoo-Dolls@users.noreply.github.com>
2023-10-23 10:49:40 -0600

    Finished Side Menu

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,17 @@
-let dropdownList = document.querySelectorAll(".dropdown-link");
+let dropdownList = document.querySelectorAll(".dropdown-link")
+let nav = document.querySelector("nav")
+let exitIcon = document.querySelector("#exit-icon")
+let hamburger = document.querySelector("#hamburger-menu")
+
+
+// Toggle Nav
+hamburger.addEventListener("click", ()=>{
+    nav.classList.toggle("active")
+})
+exitIcon.addEventListener("click", ()=>{
+    nav.classList.toggle("active")
+})
+
 
 dropdownList.forEach((element)=>{
     element.addEventListener("click", (event)=>{
